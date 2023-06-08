963b545f9e4a3d37a3a67da0985fd4238e1a2d6d
Bibek Pradhan <134042662+ADD2LIST@users.noreply.github.com>
2023-06-08 09:45:43 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+from gtts import gTTS
+
+from io import BytesIO
+
+import IPython.display as ipd
+
+st.title("Text-to-Audio Converter")
+
+text = st.text_area("Enter your text here", "")
+
+language = st.selectbox("Select Language", ("en", "es", "fr", "de", "it"))
+
+if st.button("Convert to Audio"):
+
+    if text.strip() != "":
+
+        try:
+
+            tts = gTTS(text, lang=language)
+
+            audio = BytesIO()
+
+            tts.save(audio)
+
+            audio.seek(0)
+
+            st.audio(audio, format="audio/mp3")
+
+        except:
+
+            st.error("Error: Failed to convert text to audio.")
+
+    else:
+
+        st.warning("Please enter some text.")
+
